7cb24347ab2e7ccb1afe2e070e15da38524aa32f
Lray <2322784805@qq.com>
2023-12-06 10:03:55 +0800

    Update xunfei_function.py

--- cloud_decode/xunfei_function.py
+++ cloud_decode/xunfei_function.py
@@ -38,13 +38,12 @@ from time import mktime
 import _thread as thread
 import argparse
 
-STATUS_FIRST_FRAME = 0  # 第一帧的标识
-STATUS_CONTINUE_FRAME = 1  # 中间帧标识
-STATUS_LAST_FRAME = 2  # 最后一帧的标识
+STATUS_FIRST_FRAME = 0  
+STATUS_CONTINUE_FRAME = 1  
+STATUS_LAST_FRAME = 2  
 
 
 class Ws_Param(object):
-    # 初始化
     def __init__(self, APPID, APIKey, APISecret, AudioFile, language):
         self.APPID = APPID
         self.APIKey = APIKey
@@ -58,23 +57,17 @@ class Ws_Param(object):
         # ws.errorMSG = '' //
         ###
 
-        # 公共参数(common)
         self.CommonArgs = {"app_id": self.APPID}
-        # 业务参数(business)，更多个性化参数可在官网查看
         self.BusinessArgs = {"domain": "iat", "language": self.language, "accent": "mandarin", "vinfo": 1, "vad_eos": 10000}
 
-    # 生成url
     def create_url(self):
         url = 'wss://ws-api.xfyun.cn/v2/iat'
-        # 生成RFC1123格式的时间戳
         now = datetime.now()
         date = format_date_time(mktime(now.timetuple()))
 
-        # 拼接字符串
         signature_origin = "host: " + "ws-api.xfyun.cn" + "\n"
         signature_origin += "date: " + date + "\n"
         signature_origin += "GET " + "/v2/iat " + "HTTP/1.1"
-        # 进行hmac-sha256进行加密
         signature_sha = hmac.new(self.APISecret.encode('utf-8'), signature_origin.encode('utf-8'),
                                  digestmod=hashlib.sha256).digest()
         signature_sha = base64.b64encode(signature_sha).decode(encoding='utf-8')
@@ -82,31 +75,22 @@ class Ws_Param(object):
         authorization_origin = "api_key=\"%s\", algorithm=\"%s\", headers=\"%s\", signature=\"%s\"" % (
             self.APIKey, "hmac-sha256", "host date request-line", signature_sha)
         authorization = base64.b64encode(authorization_origin.encode('utf-8')).decode(encoding='utf-8')
-        # 将请求的鉴权参数组合为字典
         v = {
             "authorization": authorization,
             "date": date,
             "host": "ws-api.xfyun.cn"
         }
-        # 拼接鉴权参数，生成url
         url = url + '?' + urlencode(v)
-        # print("date: ",date)
-        # print("v: ",v)
-        # 此处打印出建立连接时候的url,参考本demo的时候可取消上方打印的注释，比对相同参数时生成的url与自己代码生成的url是否一致
-        # print('websocket url :', url)
         return url
 
 
-# 收到websocket消息的处理
 def on_message(ws, message):
-    # print(message)
     try:
         code = json.loads(message)["code"]
         sid = json.loads(message)["sid"]
         if code != 0:
             errMsg = json.loads(message)["message"]
-            # print("sid:%s call error:%s code is:%s" % (sid, errMsg, code))
-            ws.errorMSG = "sid:%s call error:%s code is:%s" % (sid, errMsg, code)  ###########################################################
+            ws.errorMSG = "sid:%s call error:%s code is:%s" % (sid, errMsg, code) 
 
         else:
             data = json.loads(message)["data"]["result"]["ws"]
@@ -115,9 +99,7 @@ def on_message(ws, message):
             for i in data:
                 for w in i["cw"]:
                     result += w["w"]
-            # print("sid:%s call success!,data is:%s" % (sid, json.dumps(data, ensure_ascii=False)))
-            # print('result:\t' + result)
-            result = result.strip()  ####################################################################
+            result = result.strip()  
             if len(result) != 0:
                 # ws.f.write(result)
                 ws.GetMessage = 1
@@ -126,37 +108,30 @@ def on_message(ws, message):
 
     except Exception as e:
         print("receive msg,but parse exception:", e)
-        ws.errorMSG = "receive msg,but parse exception:" + str(e)  #####################################################################
+        ws.errorMSG = "receive msg,but parse exception:" + str(e) 
 
 
-# 收到websocket错误的处理
 def on_error(ws, error):
     print("### error:", error)
-    ws.errorMSG = "### error:" + str(error)  ####################################################################
+    ws.errorMSG = "### error:" + str(error)  
     pass
 
 
-# 收到websocket关闭的处理
 def on_close(ws, *kwargs):
     ws.GetMessage = 0
 
 
-# 收到websocket连接建立的处理
 def on_open(ws):
     def run(*args):
-        frameSize = 8000  # 每一帧的音频大小
-        intervel = 0.04  # 发送音频间隔(单位:s)
-        status = STATUS_FIRST_FRAME  # 音频的状态信息，标识音频是第一帧，还是中间帧、最后一帧
+        frameSize = 8000 
+        intervel = 0.04 
+        status = STATUS_FIRST_FRAME 
 
         with open(ws.AudioFile, "rb") as fp:  # wsParam.AudioFile
             while True:
                 buf = fp.read(frameSize)
-                # 文件结束
                 if not buf:
                     status = STATUS_LAST_FRAME
-                # 第一帧处理
-                # 发送第一帧音频，带business 参数
-                # appid 必须带上，只需第一帧发送
                 if status == STATUS_FIRST_FRAME:
 
                     d = {"common": ws.CommonArgs,
@@ -167,20 +142,17 @@ def on_open(ws):
                     d = json.dumps(d)
                     ws.send(d)
                     status = STATUS_CONTINUE_FRAME
-                # 中间帧处理
                 elif status == STATUS_CONTINUE_FRAME:
                     d = {"data": {"status": 1, "format": ws.sample_rate,
                                   "audio": str(base64.b64encode(buf), 'utf-8'),
                                   "encoding": "raw"}}
                     ws.send(json.dumps(d))
-                # 最后一帧处理
                 elif status == STATUS_LAST_FRAME:
                     d = {"data": {"status": 2, "format": ws.sample_rate,
                                   "audio": str(base64.b64encode(buf), 'utf-8'),
                                   "encoding": "raw"}}
                     ws.send(json.dumps(d))
                     break
-                # 模拟音频采样间隔
                 time.sleep(intervel)
 
         for i in range(ws.max_wait_time):
